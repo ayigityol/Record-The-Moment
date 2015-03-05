af4bca246999bbff06f21bdd58b76cafd68a70e6
ahmetyigityol <ayigitd@gmail.com>
2015-03-05 14:26:18 +0200

    ver1

---
+++ sictirmatik/js/main.js
@@ -0,0 +1,21 @@
+if(!localStorage.getItem("appid") || !localStorage.getItem("jsid")){
+	var appid = prompt("appid");
+	var jsid  = prompt("jsid");
+	while(appid.length != 40 || jsid.lenght != 40){
+		appid = prompt("appid");
+		jsid  = prompt("jsid");	
+	}
+	localStorage.setItem("appid",appid);
+	localStorage.setItem("jsid",jsid);
+	Parse.initialize(localStorage.setItem("appid"),localStorage.setItem("jsid"));
+}
+else {
+	Parse.initialize(localStorage.setItem("appid"),localStorage.setItem("jsid"));
+}
+function sendshit(){
+	var BOK = Parse.Object.extend("BOK");
+	var bok = new BOK();
+	bok.save({DATE: new Date()}).then(function(object) {
+	  alert("Nice one!");
+	});
+}
